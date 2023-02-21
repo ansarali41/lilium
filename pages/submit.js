663f5b54e7de3e276b32bd9212424cac4b5d8486
ansarali41 <ansar.ict14@gmail.com>
2023-02-21 13:25:31 +0600

    changes

---
+++ pages/submit.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import styles from '../styles/banner.modules.css';
+import styles from '../styles/navbar.module.css';
+
+const MyComponent = () => {
+  return (
+    <div className={styles.navLinks}>
+      home
+    </div>
+  );
+};
+
+export default MyComponent;
