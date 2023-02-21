76c4008b83cdad02c9e3c9caa9beb633d05a959d
Dibbendu Banik Purna <purna.banik164487@gmail.com>
2023-02-21 13:46:28 +0600

    banner added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import styles from '../styles/Home.module.css';
 import Header from '../Components/Header';
+import Banner from '../Components/Home/Banner';
 
 export default function Home() {
   return (
     <div className={styles.body}>
       <Header />
-      
+      <Banner />
     </div>
   );
 }
