d0019bf34f22e02c47124292b1a4ccc6d0806480
ansarali41 <ansar.ict14@gmail.com>
2023-02-24 01:07:34 +0600

    wallet connected

--- Components/MenuBar/TopNavbar.js
+++ Components/MenuBar/TopNavbar.js
@@ -1,58 +1,72 @@
 import { useEffect, useState } from 'react';
 // import "./MenuBar.css";
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import { ErgoDappConnector } from '../Requirements/Requirements';
+import { ErgoDappConnector } from '../Requirements';
+import Link from 'next/link';
+import styles from '../../styles/navbar.module.css';
+import Image from 'next/image';
+import logo from '../../assest/images/logo-tranparent.png';
+import lightIcon from '../../assest/images/light_mode.png';
 
 export default function App({ ergopay }) {
-    const [visible, setVisible] = useState(true);
-    const [ergoPay, setErgoPay] = ergopay;
-    const [walletButton, setWalletButton] = useState(true);
-    useEffect(() => {
-        if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-            setWalletButton(<ErgoDappConnector ergopayProps={[ergoPay, setErgoPay]} color={'purple'} />);
-        }
-    }, []);
+  const [visible, setVisible] = useState(true);
+  const [ergoPay, setErgoPay] = ergopay;
+  const [walletButton, setWalletButton] = useState(true);
+  useEffect(() => {
+    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      setWalletButton(<ErgoDappConnector ergopayProps={[ergoPay, setErgoPay]} color={'purple'} />);
+    }
+  }, []);
 
-    const [position, setPosition] = useState(0);
-    useEffect(() => {
-        const handleScroll = () => {
-            let moving = window.pageYOffset;
-            setVisible(position > moving);
-            setPosition(moving);
-        };
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    });
+  const [position, setPosition] = useState(0);
+  useEffect(() => {
+    const handleScroll = () => {
+      let moving = window.pageYOffset;
+      setVisible(position > moving);
+      setPosition(moving);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  });
 
-    const cls = visible ? 'visible' : 'hidden';
+  const cls = visible ? 'visible' : 'hidden';
 
-    return (
-        <nav className={cls}>
-            <Navbar collapseOnSelect expand="lg" bg="transparent" variant="dark">
-                <Container>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto"></Nav>
-                        <Nav className="navbar-links">
-                            <Nav.Link href="#home" className="nav-button">
-                                About
-                            </Nav.Link>
-                            <Nav.Link href="#showcase" className="nav-button">
-                                Showcase
-                            </Nav.Link>
-                            <Nav.Link href="#traits" className="nav-button">
-                                Traits
-                            </Nav.Link>
-                            <Nav.Link href="#creators" className="nav-button">
-                                Creators
-                            </Nav.Link>
-                        </Nav>
-                        {walletButton}
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </nav>
-    );
+  return (
+    <nav className={cls}>
+      <Navbar collapseOnSelect expand='lg' bg='transparent' variant='dark' className={styles.container}>
+        <Navbar.Brand>
+          <Link href='/' className={styles.navLinks}>
+            <Image
+              src={logo}
+              alt='logo'
+              width={300}
+              height={50}
+            />
+          </Link>
+        </Navbar.Brand>
+        <Container>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='me-auto'></Nav>
+            <Nav className={styles.navContainer}>
+              <Nav.Link href='#home' className='nav-button'>
+                <Link href='/' className={styles.navLinks}>
+                  <Image
+                    src={lightIcon}
+                    alt='Picture of the author'
+                    className={styles.lightIconStyle}
+                    width={32}
+                    height={32}
+                  />
+                </Link>
+              </Nav.Link>
+            </Nav>
+            {walletButton}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </nav>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <div className={styles.body}>
       <NavContainer />
-      <Header />
+      {/*<Header />*/}
       <Banner />
       <SellBanner />
       <HomeWork />
