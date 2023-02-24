1afcf08c365f54748d20f392beba3a14a0adf019
Dibbendu Banik Purna <purna.banik164487@gmail.com>
2023-02-24 12:29:10 +0600

    carousel and other home page fixed

--- Components/Home/Banner.js
+++ Components/Home/Banner.js
@@ -6,27 +6,31 @@ import Link from 'next/link';
 
 const Banner = () => {
   return (
-    <div className='row'>
-      <div className='col-md-6 m-5'>
-        <h1 className={styles.bannerHeaderText}>
-          THE BEST
-          <br /> COMMUNITY
-          <br /> CENTRIC NFT
-          <br /> <span className={styles.colorText}>SALE PLATFORM</span>
-        </h1>
-        <p className={styles.bannerText}>
-          LILIUM is a next-generation smart contract platform through which each
-          your audience by using our SaaS solutions to build your own custom
-          white label NFT marketplace.
-        </p>
-        <Link href='/submit' className={styles.navLinks}>
-          <button className={styles.bannerButtons}>
-            <span className={styles.navLinks}>Create Collection</span>
-          </button>
-        </Link>
-      </div>
-      <div className='col-md-5'>
-        <Image className={styles.bannerImage} src={Circle} alt='circle' />
+    <div className={styles.banner}>
+      <div className="row">
+        <div className="col-md-6">
+          <h1 className={styles.bannerHeaderText}>
+            THE BEST
+            <br /> COMMUNITY
+            <br /> CENTRIC NFT
+            <br /> <span className={styles.colorText}>SALE PLATFORM</span>
+          </h1>
+          <p className={styles.bannerText}>
+            LILIUM is a next-generation smart contract platform through which
+            each your audience by using our SaaS solutions to build your own
+            custom white label NFT marketplace.
+          </p>
+          <Link href="/submit" className={styles.navLinks}>
+            <button className={styles.bannerButtons}>
+              <span className={styles.navLinks}>Create Collection</span>
+            </button>
+          </Link>
+        </div>
+        <div className="col-md-6">
+          <div className="d-flex justify-content-center">
+            <Image className="img-fluid" src={Circle} alt="circle" />
+          </div>
+        </div>
       </div>
     </div>
   );
