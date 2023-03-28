d1ba053b63864e2aa80d1209716b86bfe05202c6
ansarali41 <ansar.ict14@gmail.com>
2023-03-28 22:16:30 +0600

    fixed

--- pages/_app.js
+++ pages/_app.js
@@ -2,23 +2,20 @@ import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Head from 'next/head';
 import Script from 'next/script';
+import '../styles/fonts.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>Lilium</title>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        {/*<link*/}
-        {/*  href="https://fonts.cdnfonts.com/css/akira-expanded"*/}
-        {/*  rel="stylesheet"*/}
-        {/*/>*/}
-
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="https://fonts.cdnfonts.com/css/akira-expanded"
+          rel="stylesheet"
+        />
       </Head>
-      <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        strategy="beforeInteractive"
-      />
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" />
       <Component {...pageProps} />
     </>
   );
